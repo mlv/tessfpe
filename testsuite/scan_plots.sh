0434cc92a4a523f9d97fb7913449d2eadb6196bb
Ed Bokhour <ebok@space.mit.edu>
2016-03-17 15:38:44 -0400

    Scan plots is smarter now...

--- testsuite/scan_plots.sh
+++ testsuite/scan_plots.sh
@@ -4,118 +4,102 @@ set -euo pipefail
 IFS=$'\n\t'
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-SAMPLES_PER_STEP=10
-STEPS=20
+# Increase these numbers to make this take longer to run
+SAMPLES_PER_STEP=2
+STEPS=5
+
+# Where to dump the plots; change this for different scripts.  Make sure it is empty before running this script
 OUTPUT_DIR=${DIR}/operating_parameter_scan_plots
-SCAN_CMD=${DIR}/venv/bin/scan_plot --steps ${STEPS} --samples-per-step ${SAMPLES_PER_STEP} --output ${OUTPUT_DIR}
+
+function scan_plot {
+  ${DIR}/venv/bin/scan_plot --steps ${STEPS} --samples-per-step ${SAMPLES_PER_STEP} --output ${OUTPUT_DIR} $*
+}
 
 make -C ${DIR} reinstall
 mkdir -p ${OUTPUT_DIR}
 
-${SCAN_CMD} ccd1_backside
-${SCAN_CMD} ccd1_input_diode_high
-${SCAN_CMD} ccd1_input_diode_low
-${SCAN_CMD} ccd1_input_gate_1
-${SCAN_CMD} ccd1_input_gate_2
-${SCAN_CMD} ccd1_output_drain_a
-${SCAN_CMD} ccd1_output_drain_a_offset
-${SCAN_CMD} ccd1_output_drain_b
-${SCAN_CMD} ccd1_output_drain_b_offset
-${SCAN_CMD} ccd1_output_drain_c
-${SCAN_CMD} ccd1_output_drain_c_offset
-${SCAN_CMD} ccd1_output_drain_d
-${SCAN_CMD} ccd1_output_drain_d_offset
-${SCAN_CMD} ccd1_output_gate
-${SCAN_CMD} ccd1_parallel_high
-${SCAN_CMD} ccd1_parallel_high_offset
-${SCAN_CMD} ccd1_parallel_low
-${SCAN_CMD} ccd1_reset_drain
-${SCAN_CMD} ccd1_reset_high
-${SCAN_CMD} ccd1_reset_low
-${SCAN_CMD} ccd1_reset_low_offset
-${SCAN_CMD} ccd1_scupper
-${SCAN_CMD} ccd1_serial_high
-${SCAN_CMD} ccd1_serial_low
-${SCAN_CMD} ccd1_serial_low_offset
-${SCAN_CMD} ccd1_substrate
-${SCAN_CMD} ccd2_backside
-${SCAN_CMD} ccd2_input_diode_high
-${SCAN_CMD} ccd2_input_diode_low
-${SCAN_CMD} ccd2_input_gate_1
-${SCAN_CMD} ccd2_input_gate_2
-${SCAN_CMD} ccd2_output_drain_a
-${SCAN_CMD} ccd2_output_drain_a_offset
-${SCAN_CMD} ccd2_output_drain_b
-${SCAN_CMD} ccd2_output_drain_b_offset
-${SCAN_CMD} ccd2_output_drain_c
-${SCAN_CMD} ccd2_output_drain_c_offset
-${SCAN_CMD} ccd2_output_drain_d
-${SCAN_CMD} ccd2_output_drain_d_offset
-${SCAN_CMD} ccd2_output_gate
-${SCAN_CMD} ccd2_parallel_high
-${SCAN_CMD} ccd2_parallel_high_offset
-${SCAN_CMD} ccd2_parallel_low
-${SCAN_CMD} ccd2_reset_drain
-${SCAN_CMD} ccd2_reset_high
-${SCAN_CMD} ccd2_reset_low
-${SCAN_CMD} ccd2_reset_low_offset
-${SCAN_CMD} ccd2_scupper
-${SCAN_CMD} ccd2_serial_high
-${SCAN_CMD} ccd2_serial_low
-${SCAN_CMD} ccd2_serial_low_offset
-${SCAN_CMD} ccd2_substrate
-${SCAN_CMD} ccd3_backside
-${SCAN_CMD} ccd3_input_diode_high
-${SCAN_CMD} ccd3_input_diode_low
-${SCAN_CMD} ccd3_input_gate_1
-${SCAN_CMD} ccd3_input_gate_2
-${SCAN_CMD} ccd3_output_drain_a
-${SCAN_CMD} ccd3_output_drain_a_offset
-${SCAN_CMD} ccd3_output_drain_b
-${SCAN_CMD} ccd3_output_drain_b_offset
-${SCAN_CMD} ccd3_output_drain_c
-${SCAN_CMD} ccd3_output_drain_c_offset
-${SCAN_CMD} ccd3_output_drain_d
-${SCAN_CMD} ccd3_output_drain_d_offset
-${SCAN_CMD} ccd3_output_gate
-${SCAN_CMD} ccd3_parallel_high
-${SCAN_CMD} ccd3_parallel_high_offset
-${SCAN_CMD} ccd3_parallel_low
-${SCAN_CMD} ccd3_reset_drain
-${SCAN_CMD} ccd3_reset_high
-${SCAN_CMD} ccd3_reset_low
-${SCAN_CMD} ccd3_reset_low_offset
-${SCAN_CMD} ccd3_scupper
-${SCAN_CMD} ccd3_serial_high
-${SCAN_CMD} ccd3_serial_low
-${SCAN_CMD} ccd3_serial_low_offset
-${SCAN_CMD} ccd3_substrate
-${SCAN_CMD} ccd4_backside
-${SCAN_CMD} ccd4_input_diode_high
-${SCAN_CMD} ccd4_input_diode_low
-${SCAN_CMD} ccd4_input_gate_1
-${SCAN_CMD} ccd4_input_gate_2
-${SCAN_CMD} ccd4_output_drain_a
-${SCAN_CMD} ccd4_output_drain_a_offset
-${SCAN_CMD} ccd4_output_drain_b
-${SCAN_CMD} ccd4_output_drain_b_offset
-${SCAN_CMD} ccd4_output_drain_c
-${SCAN_CMD} ccd4_output_drain_c_offset
-${SCAN_CMD} ccd4_output_drain_d
-${SCAN_CMD} ccd4_output_drain_d_offset
-${SCAN_CMD} ccd4_output_gate
-${SCAN_CMD} ccd4_parallel_high
-${SCAN_CMD} ccd4_parallel_high_offset
-${SCAN_CMD} ccd4_parallel_low
-${SCAN_CMD} ccd4_reset_drain
-${SCAN_CMD} ccd4_reset_high
-${SCAN_CMD} ccd4_reset_low
-${SCAN_CMD} ccd4_reset_low_offset
-${SCAN_CMD} ccd4_scupper
-${SCAN_CMD} ccd4_serial_high
-${SCAN_CMD} ccd4_serial_low
-${SCAN_CMD} ccd4_serial_low_offset
-${SCAN_CMD} ccd4_substrate
-${SCAN_CMD} heater_1_current
-${SCAN_CMD} heater_2_current
-${SCAN_CMD} heater_3_current
+# In each case, the first argument is the operating parameter, the second is the low value, the third is the high value
+# Low and high values default to listings in the data sheet if not specified
+# Output is summarized in ${OUTPUT_DIR}/summary.pdf
+
+scan_plot ccd1_backside # Doesn't work :( ...delete?
+scan_plot ccd1_input_diode_high 0 13 #14.5
+scan_plot ccd1_input_diode_low
+scan_plot ccd1_input_gate_1
+scan_plot ccd1_input_gate_2
+scan_plot ccd1_output_drain_a
+scan_plot ccd1_output_drain_b
+scan_plot ccd1_output_drain_c
+scan_plot ccd1_output_drain_d
+scan_plot ccd1_output_gate
+scan_plot ccd1_parallel_high
+scan_plot ccd1_parallel_low
+scan_plot ccd1_reset_drain
+scan_plot ccd1_reset_high
+scan_plot ccd1_reset_low
+scan_plot ccd1_scupper
+scan_plot ccd1_serial_high
+scan_plot ccd1_serial_low
+scan_plot ccd1_substrate
+scan_plot ccd2_backside
+scan_plot ccd2_input_diode_high
+scan_plot ccd2_input_diode_low
+scan_plot ccd2_input_gate_1
+scan_plot ccd2_input_gate_2
+scan_plot ccd2_output_drain_a
+scan_plot ccd2_output_drain_b
+scan_plot ccd2_output_drain_c
+scan_plot ccd2_output_drain_d
+scan_plot ccd2_output_gate
+scan_plot ccd2_parallel_high
+scan_plot ccd2_parallel_low
+scan_plot ccd2_reset_drain
+scan_plot ccd2_reset_high
+scan_plot ccd2_reset_low
+scan_plot ccd2_scupper
+scan_plot ccd2_serial_high
+scan_plot ccd2_serial_low
+scan_plot ccd2_substrate
+scan_plot ccd3_backside
+scan_plot ccd3_input_diode_high
+scan_plot ccd3_input_diode_low
+scan_plot ccd3_input_gate_1
+scan_plot ccd3_input_gate_2
+scan_plot ccd3_output_drain_a
+scan_plot ccd3_output_drain_b
+scan_plot ccd3_output_drain_c
+scan_plot ccd3_output_drain_d
+scan_plot ccd3_output_gate
+scan_plot ccd3_parallel_high
+scan_plot ccd3_parallel_low
+scan_plot ccd3_reset_drain
+scan_plot ccd3_reset_high
+scan_plot ccd3_reset_low
+scan_plot ccd3_scupper
+scan_plot ccd3_serial_high
+scan_plot ccd3_serial_low
+scan_plot ccd3_substrate
+scan_plot ccd4_backside
+scan_plot ccd4_input_diode_high
+scan_plot ccd4_input_diode_low
+scan_plot ccd4_input_gate_1
+scan_plot ccd4_input_gate_2
+scan_plot ccd4_output_drain_a
+scan_plot ccd4_output_drain_b
+scan_plot ccd4_output_drain_c
+scan_plot ccd4_output_drain_d
+scan_plot ccd4_output_gate
+scan_plot ccd4_parallel_high
+scan_plot ccd4_parallel_low
+scan_plot ccd4_reset_drain
+scan_plot ccd4_reset_high
+scan_plot ccd4_reset_low
+scan_plot ccd4_scupper
+scan_plot ccd4_serial_high
+scan_plot ccd4_serial_low
+scan_plot ccd4_substrate
+scan_plot heater_1_current
+scan_plot heater_2_current
+scan_plot heater_3_current
+
+gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -sOutputFile=${OUTPUT_DIR}/summary.pdf ${OUTPUT_DIR}/*.pdf 
