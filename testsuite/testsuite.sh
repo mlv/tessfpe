c37d5f5748976d8a82ff8c46c7926e99ea0a6b9e
Ed Bokhour <ebok@space.mit.edu>
2016-03-23 20:06:56 -0400

    Updating testsuite.sh to test scan_plot

--- testsuite/testsuite.sh
+++ testsuite/testsuite.sh
@@ -8,10 +8,11 @@ make -C ${DIR} reinstall
 
 ${PYTHON} ${DIR}/venv/bin/observatory_simulator_version
 # TODO: Load specified file?
+${PYTHON} ${DIR}/venv/bin/house_keeping
 ${PYTHON} ${DIR}/venv/bin/load_wrapper --help
-${PYTHON} ${DIR}/venv/bin/load_wrapper 6.1t.4
-${PYTHON} ${DIR}/venv/bin/load_wrapper 6.1t.4
-# TODO: Run without preloading the wrapper?
+${PYTHON} ${DIR}/venv/bin/load_wrapper 6.1t.4 --force
+${PYTHON} ${DIR}/venv/bin/load_wrapper
+${PYTHON} ${DIR}/venv/bin/load_wrapper --dhu-reset
 ${PYTHON} ${DIR}/venv/bin/house_keeping --help
 ${PYTHON} ${DIR}/venv/bin/house_keeping
 ${PYTHON} ${DIR}/venv/bin/house_keeping --samples 20 --channels pt1000_sensor_*
@@ -25,15 +26,9 @@ ${PYTHON} ${DIR}/venv/bin/upload_fpe_program ${DIR}/program.fpe
 
 ${PYTHON} ${DIR}/venv/bin/start_frames
 ${PYTHON} ${DIR}/venv/bin/observatory_simulator_version
-# TODO: Enforce status?
-${PYTHON} ${DIR}/venv/bin/frames_running_status
+${PYTHON} ${DIR}/venv/bin/frames_running_status --check-running
 ${PYTHON} ${DIR}/venv/bin/stop_frames
-${PYTHON} ${DIR}/venv/bin/frames_running_status
-
-${PYTHON} ${DIR}/venv/bin/start_frames
-sleep 5
-${PYTHON} ${DIR}/venv/bin/stop_frames
-${PYTHON} ${DIR}/venv/bin/frames_running_status
+${PYTHON} ${DIR}/venv/bin/frames_running_status --check-not-running
 ${PYTHON} ${DIR}/venv/bin/operating_parameters
 ${PYTHON} ${DIR}/venv/bin/operating_parameters --set-defaults
 ${PYTHON} ${DIR}/venv/bin/start_frames
@@ -45,8 +40,10 @@ ${PYTHON} ${DIR}/venv/bin/operating_parameters --set-defaults
 ${PYTHON} ${DIR}/venv/bin/operating_parameters ${DIR}/low_params.json
 ${PYTHON} ${DIR}/venv/bin/operating_parameters --set-defaults
 ${PYTHON} ${DIR}/venv/bin/operating_parameters --set ccd4_substrate=0.0 ccd4_output_gate=-8
-${PYTHON} ${DIR}/venv/bin/frames_running_status
+${PYTHON} ${DIR}/venv/bin/frames_running_status --check-running
 ${PYTHON} ${DIR}/venv/bin/stop_frames
+
+${PYTHON} ${DIR}/venv/bin/scan_plot ccd1_reset_low --steps 100 --output $(mktemp -u /tmp/ccd1_reset_low.XXXXXXXXXX)
 
 ${PYTHON} ${DIR}/venv/bin/tessfpe_version
 ${PYTHON} ${DIR}/venv/bin/compile_fpe_programs --help
@@ -64,9 +61,4 @@ ${PYTHON} ${DIR}/venv/bin/compile_operating_parameters < ${DIR}/low_params.json
 ${PYTHON} ${DIR}/venv/bin/compile_operating_parameters ${DIR}/low_params.json
 ${PYTHON} ${DIR}/venv/bin/compile_operating_parameters ${DIR}/low_params.json low_params.bin
 
-${PYTHON} ${DIR}/venv/bin/fpe_program_sequences --help
-${PYTHON} ${DIR}/venv/bin/fpe_program_sequences ${DIR}/program.fpe
-${PYTHON} ${DIR}/venv/bin/fpe2spicetiming --help
-for sequence in $(${PYTHON} ${DIR}/venv/bin/fpe_program_sequences ${DIR}/program.fpe) ; do
-    ${PYTHON} ${DIR}/venv/bin/fpe2spicetiming ${DIR}/program.fpe --sequence $sequence
-done
+echo "--------------------- $(tput setaf 2) ☺︎ SUCCESS! ☺︎ $(tput sgr0) ---------------------"
