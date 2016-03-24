f8ad521d33bff31b1a986fb410663316d42cf845
Ed Bokhour <ebok@space.mit.edu>
2016-03-23 21:23:41 -0400

    Writing plots to temp directory now

--- testsuite/scan_plots.sh
+++ testsuite/scan_plots.sh
@@ -3,19 +3,27 @@
 set -euo pipefail
 IFS=$'\n\t'
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+RED="$(tput setaf 1)"
+GREEN="$(tput setaf 2)"
+RESET="$(tput sgr0)"
+
+if [ -f "summary.pdf" ] ; then
+   echo "${RED}Please make a backup of summary.pdf BEFORE running this program!${RESET}" > /dev/stderr
+   exit 1
+fi
 
 # Increase these numbers to make this take longer to run
-SAMPLES_PER_STEP=2
-STEPS=5
+SAMPLES_PER_STEP=5
+STEPS=20
 
 # Where to dump the plots; change this for different scripts.  Make sure it is empty before running this script
-OUTPUT_DIR=${DIR}/operating_parameter_scan_plots
+OUTPUT_DIR=$(mktemp -d)
 
 function scan_plot {
   ${DIR}/venv/bin/scan_plot --steps ${STEPS} --samples-per-step ${SAMPLES_PER_STEP} --output ${OUTPUT_DIR} $*
 }
 
-make -C ${DIR} reinstall
+make -C ${DIR} reinstall > /dev/null 2>&1
 mkdir -p ${OUTPUT_DIR}
 
 # In each case, the first argument is the operating parameter, the second is the low value, the third is the high value
@@ -23,7 +31,7 @@ mkdir -p ${OUTPUT_DIR}
 # Output is summarized in ${OUTPUT_DIR}/summary.pdf
 
 scan_plot ccd1_backside # Doesn't work :( ...delete?
-scan_plot ccd1_input_diode_high 0 13 #14.5
+scan_plot ccd1_input_diode_high 0 14.5
 scan_plot ccd1_input_diode_low
 scan_plot ccd1_input_gate_1
 scan_plot ccd1_input_gate_2
@@ -102,4 +110,5 @@ scan_plot heater_1_current
 scan_plot heater_2_current
 scan_plot heater_3_current
 
-gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -sOutputFile=${OUTPUT_DIR}/summary.pdf ${OUTPUT_DIR}/*.pdf 
+gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -sOutputFile=summary.pdf ${OUTPUT_DIR}/*.pdf 
+echo Wrote summary.pdf ${GREEN}ok${RESET}
