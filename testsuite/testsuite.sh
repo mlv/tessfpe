69d29e4a5ad72d89e741e3f87d82d109f798eeb4
Ed Bokhour <ebok@space.mit.edu>
2016-05-17 12:09:03 -0400

    Now allowing the user to switch which FPE they want to test

--- testsuite/testsuite.sh
+++ testsuite/testsuite.sh
@@ -3,47 +3,50 @@
 set -euo pipefail
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 PYTHON=${DIR}/tessfpe/bin/python
+FPE_NUMBER=${FPE_NUMBER-1}
 
 make -C ${DIR} reinstall
 
+FPE_HOSTNAME=observatorysim
 ${PYTHON} ${DIR}/tessfpe/bin/observatory_simulator_version
+${PYTHON} ${DIR}/tessfpe/bin/house_keeping --FPE-number $FPE_NUMBER
+#${PYTHON} ${DIR}/tessfpe/bin/upload_fpe_program UpAndRunning --FPE-number $FPE_NUMBER
 # TODO: Load specified file?
-${PYTHON} ${DIR}/tessfpe/bin/house_keeping
-${PYTHON} ${DIR}/tessfpe/bin/load_wrapper --help
-${PYTHON} ${DIR}/tessfpe/bin/load_wrapper 6.1t.4 --force
-${PYTHON} ${DIR}/tessfpe/bin/load_wrapper
-${PYTHON} ${DIR}/tessfpe/bin/load_wrapper --dhu-reset
-${PYTHON} ${DIR}/tessfpe/bin/house_keeping --help
-${PYTHON} ${DIR}/tessfpe/bin/house_keeping
-${PYTHON} ${DIR}/tessfpe/bin/house_keeping --samples 20 --channels pt1000_sensor_*
-${DIR}/tessfpe/bin/voltage_test --samples 20
-${DIR}/tessfpe/bin/bias_test --samples 20
-${DIR}/tessfpe/bin/rtd_test --samples 20
+${PYTHON} ${DIR}/tessfpe/bin/load_wrapper --help --FPE-number $FPE_NUMBER
+${PYTHON} ${DIR}/tessfpe/bin/load_wrapper --force --FPE-number $FPE_NUMBER
+${PYTHON} ${DIR}/tessfpe/bin/load_wrapper --FPE-number $FPE_NUMBER
+${PYTHON} ${DIR}/tessfpe/bin/load_wrapper --dhu-reset --FPE-number $FPE_NUMBER
+${PYTHON} ${DIR}/tessfpe/bin/house_keeping --help --FPE-number $FPE_NUMBER
+${PYTHON} ${DIR}/tessfpe/bin/house_keeping --FPE-number $FPE_NUMBER
+${PYTHON} ${DIR}/tessfpe/bin/house_keeping --samples 20 --channels pt1000_sensor_* --FPE-number $FPE_NUMBER
+${DIR}/tessfpe/bin/voltage_test --samples 20 --FPE-number $FPE_NUMBER --FPE-number $FPE_NUMBER
+${DIR}/tessfpe/bin/bias_test --samples 20 --FPE-number $FPE_NUMBER --FPE-number $FPE_NUMBER
+${DIR}/tessfpe/bin/rtd_test --samples 20 --FPE-number $FPE_NUMBER --FPE-number $FPE_NUMBER
 # TODO: Upload binary files?
 ${PYTHON} ${DIR}/tessfpe/bin/upload_fpe_program --help
-${PYTHON} ${DIR}/tessfpe/bin/upload_fpe_program < ${DIR}/program.fpe
-${PYTHON} ${DIR}/tessfpe/bin/upload_fpe_program ${DIR}/program.fpe
+${PYTHON} ${DIR}/tessfpe/bin/upload_fpe_program --FPE-number $FPE_NUMBER < ${DIR}/program.fpe 
+${PYTHON} ${DIR}/tessfpe/bin/upload_fpe_program ${DIR}/program.fpe --FPE-number $FPE_NUMBER
 
-${PYTHON} ${DIR}/tessfpe/bin/start_frames
-${PYTHON} ${DIR}/tessfpe/bin/observatory_simulator_version
-${PYTHON} ${DIR}/tessfpe/bin/frames_running_status --check-running
-${PYTHON} ${DIR}/tessfpe/bin/stop_frames
-${PYTHON} ${DIR}/tessfpe/bin/frames_running_status --check-not-running
-${PYTHON} ${DIR}/tessfpe/bin/operating_parameters
-${PYTHON} ${DIR}/tessfpe/bin/operating_parameters --set-defaults
-${PYTHON} ${DIR}/tessfpe/bin/start_frames
+${PYTHON} ${DIR}/tessfpe/bin/start_frames --FPE-number $FPE_NUMBER
+${PYTHON} ${DIR}/tessfpe/bin/observatory_simulator_version --FPE-number $FPE_NUMBER
+${PYTHON} ${DIR}/tessfpe/bin/frames_running_status --check-running --FPE-number $FPE_NUMBER
+${PYTHON} ${DIR}/tessfpe/bin/stop_frames --FPE-number $FPE_NUMBER
+${PYTHON} ${DIR}/tessfpe/bin/frames_running_status --check-not-running --FPE-number $FPE_NUMBER
+${PYTHON} ${DIR}/tessfpe/bin/operating_parameters --FPE-number $FPE_NUMBER
+${PYTHON} ${DIR}/tessfpe/bin/operating_parameters --set-defaults --FPE-number $FPE_NUMBER
+${PYTHON} ${DIR}/tessfpe/bin/start_frames --FPE-number $FPE_NUMBER
 # TODO: Load from binary file?
-${PYTHON} ${DIR}/tessfpe/bin/operating_parameters
-${PYTHON} ${DIR}/tessfpe/bin/operating_parameters --set-defaults
-${PYTHON} ${DIR}/tessfpe/bin/operating_parameters < ${DIR}/low_params.json
-${PYTHON} ${DIR}/tessfpe/bin/operating_parameters --set-defaults
-${PYTHON} ${DIR}/tessfpe/bin/operating_parameters ${DIR}/low_params.json
-${PYTHON} ${DIR}/tessfpe/bin/operating_parameters --set-defaults
-${PYTHON} ${DIR}/tessfpe/bin/operating_parameters --set ccd4_substrate=0.0 ccd4_output_gate=-8
-${PYTHON} ${DIR}/tessfpe/bin/frames_running_status --check-running
-${PYTHON} ${DIR}/tessfpe/bin/stop_frames
+${PYTHON} ${DIR}/tessfpe/bin/operating_parameters --FPE-number $FPE_NUMBER
+${PYTHON} ${DIR}/tessfpe/bin/operating_parameters --set-defaults --FPE-number $FPE_NUMBER
+${PYTHON} ${DIR}/tessfpe/bin/operating_parameters --FPE-number $FPE_NUMBER < ${DIR}/low_params.json
+${PYTHON} ${DIR}/tessfpe/bin/operating_parameters --set-defaults --FPE-number $FPE_NUMBER
+${PYTHON} ${DIR}/tessfpe/bin/operating_parameters ${DIR}/low_params.json --FPE-number $FPE_NUMBER
+${PYTHON} ${DIR}/tessfpe/bin/operating_parameters --set-defaults --FPE-number $FPE_NUMBER
+${PYTHON} ${DIR}/tessfpe/bin/operating_parameters --set ccd4_substrate=0.0 ccd4_output_gate=-8 --FPE-number $FPE_NUMBER
+${PYTHON} ${DIR}/tessfpe/bin/frames_running_status --check-running --FPE-number $FPE_NUMBER
+${PYTHON} ${DIR}/tessfpe/bin/stop_frames --FPE-number $FPE_NUMBER
 
-${PYTHON} ${DIR}/tessfpe/bin/scan_plot ccd1_reset_low --steps 100 --output $(mktemp -u /tmp/ccd1_reset_low.XXXXXXXXXX)
+#${PYTHON} ${DIR}/tessfpe/bin/scan_plot ccd1_reset_low --steps 100 --output $(mktemp -u /tmp/ccd1_reset_low.XXXXXXXXXX)
 
 ${PYTHON} ${DIR}/tessfpe/bin/tessfpe_version
 ${PYTHON} ${DIR}/tessfpe/bin/compile_fpe_programs --help
